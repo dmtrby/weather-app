9fbc0777ea73c93fa20ede33210c456bdb29158f
dmtrby <dmitry.mahliui@gmail.com>
2019-08-22 00:13:23 +0300

    weather app on react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import WeatherDisplay from './components/WeatherDisplay';
+
+const PLACES = [
+  { name: "Palo Alto", zip: "94303" },
+  { name: "San Jose", zip: "94088" },
+  { name: "Santa Cruz", zip: "95062" },
+  { name: "Honolulu", zip: "96803" }
+];
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeCity: 0,
+    }
+  }
+
+  changeCity = (e) => {
+    const newCity = e.target.getAttribute('cityid');
+    console.log(newCity);
+    this.setState({
+      activeCity: newCity,
+    })
+  }
+
+  render() {
+    console.log('RENDERING');
+    return (
+      <div className="App">
+        <h1>Let's display some weather!</h1>
+        <div>
+          {PLACES.map((v, i) => (
+            <button
+              key={i}
+              className="btn"
+              cityid={i}
+              onClick={this.changeCity}
+            >{v.name}</button>
+          ))}
+        </div>
+        <WeatherDisplay key={this.state.activeCity} zip={PLACES[this.state.activeCity].zip}></WeatherDisplay>
+      </div>
+    );
+  }
+
 }
 
 export default App;
